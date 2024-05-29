ad21f69381e8167e38d5fc62325d5f1e002878f5
Yokeyntrra.S <137605923+Yokey-01@users.noreply.github.com>
2024-05-29 09:39:15 +0530

    Update urls.py

--- app1/urls.py
+++ app1/urls.py
@@ -3,17 +3,17 @@ from django.urls import path
 from . import views
   
 urlpatterns = [
-    path("index/", views.index,name='index'),
-    path("index/login2/", views.login2,name='login2'),
-    path("index/login2/signin/", views.signin,name='signin'),
+    path("", views.index,name='index'),
+    path("login2/", views.login2,name='login2'),
+    path("login2/signin/", views.signin,name='signin'),
     # path("index/login2/signin/incorrect", views.incorrect,name='login2'),
-    path("index/login2/signup/", views.sign,name='sign'),
-    path("index/login2/signup/back/",views.back,name='back'),
-    path('index/login2/signup/createuser/',views.createuser,name="createuser"),
-    path("index/login2/signin/predict/", views.predict,name='predict'),
-    path("index/login2/signin/predict/result", views.result),
-    path("index/login2/signin/predict/result/output/", views.output),
-    path("index/login2/signin/predict/home/", views.home),
-    path("index/login2/signin/predict/home/back2/", views.back2),
+    path("login2/signup/", views.sign,name='sign'),
+    path("login2/signup/back/",views.back,name='back'),
+    path('login2/signup/createuser/',views.createuser,name="createuser"),
+    path("login2/signin/predict/", views.predict,name='predict'),
+    path("login2/signin/predict/result", views.result),
+    path("login2/signin/predict/result/output/", views.output),
+    path("login2/signin/predict/home/", views.home),
+    path("login2/signin/predict/home/back2/", views.back2),
     # path("index/login2/predict/data/",views.data,name ="data"),
 ]
